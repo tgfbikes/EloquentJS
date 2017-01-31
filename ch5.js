e1a2b34b608f03d7365d6a43d8e2b61935d8f783
Stephen King <stephenking@Stephens-MacBook-Pro.local>
2017-01-30 20:17:06 -0700

    finished chapter 5 exercises

--- ch5.js
+++ ch5.js
@@ -1,6 +1,9 @@
 'use strict'
 
 let ancestryData = require('./ancestry.js')
+let byName = {}
+
+ancestryData.forEach((person) => byName[person.name] = person)
 
 // Flatteniing
 let arrayOfarrays = [
@@ -12,12 +15,9 @@ let arrayOfarrays = [
 
 let flattenedArray = arrayOfarrays.reduce((array1, array2) => array1.concat(array2))
 
-//console.log(flattenedArray)
+console.log(flattenedArray)
 
 // Mother-child age difference
-let byName = {}
-ancestryData.forEach((person) => byName[person.name] = person)
-
 function averageOfItemsInArray (someArray) {
 	return someArray.reduce((val1, val2) => val1 + val2) / someArray.length
 }
@@ -36,4 +36,63 @@ ancestryData.forEach((person) => {
 })
 
 console.log(averageOfItemsInArray(mothersAgeArray))
+
+// Historical life expectancy
+function groupByAges (someArray) {
+	let groupsOfAges = {}
+
+	someArray.forEach(function (person) {
+		let propName = Math.ceil(person.died / 100)
+
+		if (groupsOfAges[propName]) {
+			groupsOfAges[propName].push(person.died - person.born)
+		} else {
+			groupsOfAges[propName] = []
+			groupsOfAges[propName].push(person.died - person.born)
+		}
+	})
+
+	return groupsOfAges
+}
+
+let averageAgesPerCentury = groupByAges(ancestryData)
+
+for (let key in averageAgesPerCentury) {
+	let array = averageAgesPerCentury[key]
+
+	averageAgesPerCentury[key] = array.reduce((a, b) => a + b) / array.length
+}
+
+console.log(averageAgesPerCentury)
+
+
+// Every and then some
+function every (someArray, predicateFunc) {
+	for (let i = 0; i < someArray.length; i++) {
+		if (!predicateFunc(someArray[i])) return false
+	}
+	return true
+}
+
+function some (someArray, predicateFunc) {
+	for (let i = 0; i < someArray.length; i++) {
+		if (predicateFunc(someArray[i])) return true
+	}
+	return false
+}
+
+let testArray1 = [10, 20, 5, 15]
+let testArray2 = [3, 4, 6, 50]
+let testFunc = (element) => element % 5 === 0
+
+console.log(every(testArray1, testFunc))
+console.log(some(testArray2, testFunc))
+
+
+
+
+
+
+
+
 
